712e7c99b1029d567087553a28ff690253e11b65
palnyseth <paalnyseth@gmail.com>
2025-05-25 12:49:21 +0900

    basic lexer v1

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module kursiv.no/interpreter
+module kursiv/interpreter
 
 go 1.24.3
---
+++ token/token.go
@@ -0,0 +1,53 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//IDENTIFIERS + LITERALS
+	IDENT = "IDENT" //add, x, y, ....
+	INT   = "INT"   //12346
+
+	//OPERATORS
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	//DELIMITERS
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//KEYWORDS
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
