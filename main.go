1949a96b5de957747e230766f87334295263e971
palnyseth <paalnyseth@gmail.com>
2025-05-25 13:17:05 +0900

    chapter 1 done

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"kursiv/interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s!\n", user.Username)
+	fmt.Printf("Please feel free to type commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
